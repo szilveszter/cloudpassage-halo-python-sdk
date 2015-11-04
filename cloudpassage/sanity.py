945b665165cd122ff74d5b8a1729a25989533d1c
Ash Wilson <ash.d.wilson@gmail.com>
2015-11-04 15:26:25 -0800

    COE-20 Added updateServerGroup() w/ sanity checking

--- cloudpassage/sanity.py
+++ cloudpassage/sanity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #Validation routines for cpapi
 
-def validate_server_args(a):
+def validate_servergroup_create_args(a):
     val_struct = {"firewall_policy_id": unicode,
                   "linux_firewall_policy_id": unicode,
                   "windows_firewall_policy_id": unicode,
@@ -17,6 +17,32 @@ def validate_server_args(a):
     for k, v in a.items():
         if k in val_struct:
             if isinstance(v, val_struct[k]):
+                continue
+            else:
+                raise TypeError("Type incorrect for %s.  Is %s and should be %s." % (k, type(v), val_struct[k]))
+        else:
+            raise KeyError("Invalid server group attribute: %s") % k
+    return(True)
+
+def validate_servergroup_update_args(a):
+    val_struct = {"firewall_policy_id": unicode,
+                  "linux_firewall_policy_id": unicode,
+                  "windows_firewall_policy_id": unicode,
+                  "policy_ids": list,
+                  "windows_policy_ids": list,
+                  "fim_policy_ids": list,
+                  "linux_fim_policy_ids": list,
+                  "windows_fim_policy_ids": list,
+                  "lids_policy_ids": list,
+                  "tag": unicode,
+                  "name": unicode,
+                  "events_policy": unicode,
+                  "alert_profiles": list}
+    for k, v in a.items():
+        if k in val_struct:
+            if isinstance(v, val_struct[k]):
+                continue
+            elif ((val_struct[k] == unicode) and (v == None)):
                 continue
             else:
                 raise TypeError("Type incorrect for %s.  Is %s and should be %s." % (k, type(v), val_struct[k]))
