73ac972333dfa10aa700c04cf64fcdc079cd5c0f
Ash Wilson <ash.d.wilson@gmail.com>
2015-11-16 17:46:09 -0800

    coe-44 add Server.retire()

--- cloudpassage/server.py
+++ cloudpassage/server.py
@@ -1,5 +1,21 @@
+from http_helper import HttpHelper
+
+
 class Server:
 
     def __init__(self, session):
         self.session = session
         return None
+
+    def retire(self, server_id):
+        endpoint = "/v1/servers/%s" % server_id
+        body = {"server":
+                {"retire": True}}
+        request = HttpHelper(self.session)
+        try:
+            response = request.put(endpoint, body)
+        except put.CloudPassageAuthorization as e:
+            raise CloudPassageAuthorization(e.msg)
+        except put.CloudPassageValdiation as e:
+            raise CloudPassageValidation(e.msg)
+        return True
