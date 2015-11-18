fe62567e5a041e41c2245c4698366b4d08d6c668
Ash Wilson <ash.d.wilson@gmail.com>
2015-11-17 16:51:46 -0800

    coe-63 Added scan initiator module.  Some integration tests will be fulfilled by coe-66

--- cloudpassage/scan.py
+++ cloudpassage/scan.py
@@ -1,5 +1,53 @@
+from http_helper import HttpHelper
+from exceptions import CloudPassageValidation
+from exceptions import CloudPassageResourceExistence
+
+
 class Scan:
 
     def __init__(self, session):
         self.session = session
         return None
+
+    def initiate_scan(self, server_id, scan_type):
+        """Initiate a scan on a specific server.
+
+        server_id    -- Server ID (not name)
+        scan_type    -- Scan type to initiate.
+
+        Valid scan types:
+          sca  - Configuration scan
+          csm  - Configuration scan (same as sca)
+          svm  - Software vulnerability scan
+          sva  - Software vulnerability scan (same as svm)
+          sam  - Server access management scan
+          fim  - File integrity monitoring scan
+          sv   - Agent self-verifiation scan
+
+        Returns the metadata from the command issued,
+        in a dict.
+        Failure throws an exception.
+        """
+
+        supported_scan_mapping = {"sca": "sca",
+                                  "csm": "sca",
+                                  "svm": "svm",
+                                  "sva": "svm",
+                                  "sam": "sam",
+                                  "fim": "fim",
+                                  "sv": "sv"}
+        if scan_type not in supported_scan_mapping:
+            exception_message = "Unsupported scan type: %s" % scan_type
+            raise CloudPassageValidation(exception_message)
+        else:
+            scan_type_valid = supported_scan_mapping[scan_type]
+            request_body = {"scan": {"module": scan_type_valid}}
+            endpoint = "/v1/servers/%s/scans" % server_id
+            try:
+                request = HttpHelper(self.session)
+                response = request.post(endpoint, request_body)
+                command_info = response["command"]
+                return(command_info)
+            except CloudPassageResourceExistence as e:
+                print e.msg
+                raise CloudPassageResourceExistence(e.msg)
--- tests/tests/test_scan.py
+++ tests/tests/test_scan.py
@@ -17,9 +17,11 @@ proxy_host = '190.109.164.81'
 proxy_port = '1080'
 
 file, filename, data = imp.find_module('cloudpassage', [module_path])
+cp = imp.load_module('cloudpassage', file, filename, data)
 halo = imp.load_module('halo', file, filename, data)
-scan = imp.load_module('scan', file,
-                       filename, data)
+scan = imp.load_module('scan', file, filename, data)
+server_group = imp.load_module('server_group', file, filename, data)
+server = imp.load_module('server', file, filename, data)
 
 
 class TestScan:
@@ -31,3 +33,77 @@ class TestScan:
     def test_instantiation(self):
         session = halo.HaloSession(key_id, secret_key)
         assert scan.Scan(session)
+
+    def test_bad_scan_type(self):
+        rejected = False
+        session = halo.HaloSession(key_id, secret_key)
+        scanner = scan.Scan(session)
+        s_group = server_group.ServerGroup(session)
+        scan_type = "barfola"
+        server_id = s_group.list_all()[0]["id"]
+        try:
+            command = scanner.initiate_scan(server_id, scan_type)
+        except scan.CloudPassageValidation:
+            rejected = True
+        assert rejected
+
+    def test_bad_server_id(self):
+        rejected = False
+        session = halo.HaloSession(key_id, secret_key)
+        scanner = scan.Scan(session)
+        scan_type = "svm"
+        server_id = "ABC123"
+        try:
+            command = scanner.initiate_scan(server_id, scan_type)
+        except scan.CloudPassageResourceExistence:
+            rejected = True
+        assert rejected
+
+    """These are integration tests, and require the completion of the
+    server.Server module, to get a serverID for a target to initiate a
+    scan against.
+
+    def test_sv(self):
+        session = halo.HaloSession(key_id, secret_key)
+        s_group = server_group.ServerGroup(session)
+        server_group_list = s_group.list_all()
+        scanner = scan.Scan(session)
+        command = scanner.initiate_scan(server_id, "sv")
+        assert command["id"]
+
+    def test_sca(self):
+        sca_aliases = ["sca", "csm"]
+        session = halo.HaloSession(key_id, secret_key)
+        s_group = server_group.ServerGroup(session)
+        server_group_list = s_group.list_all()
+        scanner = scan.Scan(session)
+        for alias in sca_aliases:
+            command = scanner.initiate_scan(server_id, alias)
+            assert command["id"]
+
+    def test_fim(self):
+        session = halo.HaloSession(key_id, secret_key)
+        s_group = server_group.ServerGroup(session)
+        server_id = s_group.list_all()[0]["id"]
+        scanner = scan.Scan(session)
+        command = scanner.initiate_scan(server_id, 'fim')
+        assert command["id"]
+
+    def test_svm(self):
+        svm_aliases = ["svm", "sva"]
+        session = halo.HaloSession(key_id, secret_key)
+        s_group = server_group.ServerGroup(session)
+        server_id = s_group.list_all()[0]["id"]
+        scanner = scan.Scan(session)
+        for alias in svm_aliases:
+            command = scanner.initiate_scan(server_id, alias)
+            assert command["id"]
+
+    def test_sam(self):
+        session = halo.HaloSession(key_id, secret_key)
+        s_group = server_group.ServerGroup(session)
+        server_id = s_group.list_all()[0]["id"]
+        scanner = scan.Scan(session)
+        command = scanner.initiate_scan(server_id, "sam")
+        assert command["id"]
+    """
