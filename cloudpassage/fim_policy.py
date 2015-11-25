0bb8bc5c76bc27dc5f0ff532f5bd94e5a2631a74
Ash Wilson <ash.d.wilson@gmail.com>
2015-11-24 17:23:07 -0800

    coe-72 Wrapping up FIM module

--- cloudpassage/fim_policy.py
+++ cloudpassage/fim_policy.py
@@ -1,5 +1,147 @@
+from http_helper import HttpHelper
+import fn
+
+
 class FimPolicy:
 
     def __init__(self, session):
         self.session = session
         return None
+
+    def list_all(self):
+        """Returns a list of all FIM policies"""
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies"
+        key = "fim_policies"
+        max_pages = 30
+        response = request.get_paginated(endpoint, key, max_pages)
+        return(response)
+
+    def describe(self, fim_policy_id):
+        """Returns the body of the policy indicated by policy_id.
+
+        If used with update(), make sure to enclose like so:
+        {"policy": policy_body}
+        """
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s" % fim_policy_id
+        response = request.get(endpoint)
+        result = response["fim_policy"]
+        return(result)
+
+    def create(self, fim_policy_body):
+        """Creates a FIM policy from JSON document.
+
+        Returns the ID of the new policy
+        """
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/"
+        # Make sure it's the right kind of policy
+        policy_metadata = fn.determine_policy_metadata(fim_policy_body)
+        request_body = fn.policy_to_dict(fim_policy_body)
+        policy_type = policy_metadata["policy_type"]
+        if policy_type is not "FIM":
+            error_message = "Policy type is not FIM"
+            raise CloudPassageValidation(error_message)
+        else:
+            response = request.post(endpoint, request_body)
+            policy_id = response["fim_policy"]["id"]
+            return(policy_id)
+
+    def delete(self, fim_policy_id):
+        """Delete a FIM policy by ID"""
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s" % fim_policy_id
+        response = request.delete(endpoint)
+        return(None)
+
+    def update(self, fim_policy_body):
+        """Update a FIM policy.  Success returns None"""
+
+        request = HttpHelper(self.session)
+        # Make sure it's the right kind of policy
+        policy_metadata = fn.determine_policy_metadata(fim_policy_body)
+        request_body = fn.policy_to_dict(fim_policy_body)
+        policy_type = policy_metadata["policy_type"]
+        if policy_type is not "FIM":
+            error_message = "Policy type is not FIM"
+            raise self.CloudPassageValidation(error_message)
+        else:
+            fim_policy_id = request_body["fim_policy"]["id"]
+            endpoint = "/v1/fim_policies/%s" % fim_policy_id
+            response = request.put(endpoint, request_body)
+            return(None)
+
+
+class FimBaseline:
+
+    def __init__(self, session):
+        self.session = session
+        return None
+
+    def list_all(self, fim_policy_id):
+        """Returns a list of all baselines for the indicated FIM policy"""
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s/baselines" % fim_policy_id
+        key = "baselines"
+        max_pages = 30
+        response = request.get_paginated(endpoint, key, max_pages)
+        return(response)
+
+    def describe(self, fim_policy_id, fim_baseline_id):
+        """Returns the body of the baseline indicated by fim_baseline_id.
+
+        """
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s/baselines/%s" % (fim_policy_id,
+                                                         fim_baseline_id)
+        response = request.get(endpoint)
+        result = response["baseline"]
+        return(result)
+
+    def create(self, fim_policy_id, server_id, **kwargs):
+        """Creates a FIM baseline
+
+        kwargs:
+        expires   -- Number of days from today, when baseline expires
+        comment   -- Something meaningful in string form
+        Returns the ID of the new baseline
+        """
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s/baselines" % fim_policy_id
+        request_body = {"baseline": {"server_id": server_id,
+                                     "expires": None,
+                                     "comment": None}}
+        if "expires" in kwargs:
+            request_body["expires"] = kwargs["expires"]
+        if "comment" in kwargs:
+            request_body["comment"] = kwargs["comment"]
+        response = request.post(endpoint, request_body)
+        policy_id = response["baseline"]["id"]
+        return(policy_id)
+
+    def delete(self, fim_policy_id, fim_baseline_id):
+        """Delete a FIM baseline by ID"""
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s/baselines/%s" % (fim_policy_id,
+                                                         fim_baseline_id)
+        response = request.delete(endpoint)
+        return(None)
+
+    def update(self, fim_policy_id, fim_baseline_id, server_id):
+        """Update a FIM policy.  Success returns None"""
+
+        request = HttpHelper(self.session)
+        endpoint = "/v1/fim_policies/%s/baselines/%s" % (fim_policy_id,
+                                                         fim_baseline_id)
+        request_body = {"baseline": {"server_id": server_id}}
+        response = request.put(endpoint, request_body)
+        return(None)
--- tests/tests/test_fim_policy.py
+++ tests/tests/test_fim_policy.py
@@ -1,13 +1,16 @@
+import cloudpassage
 import pytest
 import pep8
 import imp
 import json
 import os
 
-module_path = os.path.abspath('../')
+# module_path = os.path.abspath('../')
 
 file_location = os.path.abspath('../cloudpassage/fim_policy.py')
 this_file = os.path.abspath(__file__)
+policy_file = os.path.abspath('./policies/' +
+                              'core-system-files-centos-v1.fim.json')
 
 key_id = os.environ.get('HALO_KEY_ID')
 secret_key = os.environ.get('HALO_SECRET_KEY')
@@ -16,18 +19,115 @@ api_hostname = os.environ.get('HALO_API_HOSTNAME')
 proxy_host = '190.109.164.81'
 proxy_port = '1080'
 
-file, filename, data = imp.find_module('cloudpassage', [module_path])
-halo = imp.load_module('halo', file, filename, data)
-fim_policy = imp.load_module('fim_policy', file,
-                             filename, data)
+# file, filename, data = imp.find_module('cloudpassage', [module_path])
+# halo = imp.load_module('halo', file, filename, data)
+# fim_policy = imp.load_module('fim_policy', file, filename, data)
 
 
 class TestFimPolicy:
+    def build_fim_policy_object(self):
+        session = cloudpassage.HaloSession(key_id, secret_key)
+        return_obj = cloudpassage.FimPolicy(session)
+        return(return_obj)
+
     def test_pep8(self):
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files([file_location, this_file])
         assert result.total_errors == 0
 
     def test_instantiation(self):
-        session = halo.HaloSession(key_id, secret_key)
-        assert fim_policy.FimPolicy(session)
+        session = cloudpassage.HaloSession(key_id, secret_key)
+        assert cloudpassage.FimPolicy(session)
+
+    def test_list_all(self):
+        request = self.build_fim_policy_object()
+        response = request.list_all()
+        assert "id" in response[0]
+
+    def test_get_details(self):
+        request = self.build_fim_policy_object()
+        policy_list = request.list_all()
+        target_policy_id = policy_list[0]["id"]
+        target_policy_body = request.describe(target_policy_id)
+        assert "id" in target_policy_body
+
+    def test_fim_policy_create_delete(self):
+        deleted = False
+        policy_retrieved = {"fim_policy": None}
+        request = self.build_fim_policy_object()
+        newname = "Functional Test Name Change"
+        with open(policy_file, 'r') as policy_file_object:
+            policy_body = policy_file_object.read()
+        policy_id = request.create(policy_body)
+        request.delete(policy_id)
+        try:
+            request.describe(policy_id)
+        except cloudpassage.CloudPassageResourceExistence:
+            deleted = True
+        assert deleted
+
+    def test_fim_policy_create_update_delete(self):
+        deleted = False
+        policy_retrieved = {"fim_policy": None}
+        request = self.build_fim_policy_object()
+        newname = "Functional Test Name Change"
+        with open(policy_file, 'r') as policy_file_object:
+            policy_body = policy_file_object.read()
+        policy_id = request.create(policy_body)
+        policy_update = json.loads(policy_body)
+        policy_update["fim_policy"]["name"] = newname
+        policy_update["fim_policy"]["id"] = policy_id
+        request.update(policy_update)
+        request.delete(policy_id)
+        try:
+            request.describe(policy_id)
+        except cloudpassage.CloudPassageResourceExistence:
+            deleted = True
+        assert deleted
+
+
+class TestFimBaseline:
+    def build_fim_baseline_object(self):
+        session = cloudpassage.HaloSession(key_id, secret_key)
+        return_obj = cloudpassage.FimBaseline(session)
+        return(return_obj)
+
+    def build_fim_policy_object(self):
+        session = cloudpassage.HaloSession(key_id, secret_key)
+        return_obj = cloudpassage.FimPolicy(session)
+        return(return_obj)
+
+    def get_active_linux_policy(self):
+        request = self.build_fim_policy_object()
+        response = request.list_all()
+        target_id = None
+        for policy in response:
+            if (policy["active"] == True and policy["platform"] == "linux"):
+                target_id = policy["id"]
+                break
+        assert target_id is not None
+        return target_id
+
+    def test_pep8(self):
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files([file_location, this_file])
+        assert result.total_errors == 0
+
+    def test_instantiation(self):
+        session = cloudpassage.HaloSession(key_id, secret_key)
+        assert cloudpassage.FimBaseline(session)
+
+    def test_list_all_baselines_for_policy(self):
+        baseline_request = self.build_fim_baseline_object()
+        target_policy_id = self.get_active_linux_policy()
+        baseline_response = baseline_request.list_all(target_policy_id)
+        assert "id" in baseline_response[0]
+
+    def test_get_details_of_baseline(self):
+        baseline_request = self.build_fim_baseline_object()
+        target_policy_id = self.get_active_linux_policy()
+        baseline_list = baseline_request.list_all(target_policy_id)
+        target_baseline_id = baseline_list[0]["id"]
+        baseline_details = baseline_request.describe(target_policy_id,
+                                                     target_baseline_id)
+        assert "id" in baseline_details
