3225ac1b3deb7eba3718ba6c2f02b0cbd6837cd0
Jye Lee <jlee@cloudpassage.com>
2016-09-02 11:36:36 -0700

    CS-2 fix alert_profile, does not have a self.policy_key

--- cloudpassage/alert_profile.py
+++ cloudpassage/alert_profile.py
@@ -33,3 +33,33 @@ class AlertProfile(Policy):
     def pagination_key(cls):
         """Defines the pagination key for parsing paged results"""
         return AlertProfile.policies
+
+    def create(self, policy_body):
+        """Creates a policy from JSON document.
+
+        Returns the ID of the new policy
+        """
+
+        request = HttpHelper(self.session)
+        request_body = utility.policy_to_dict(policy_body)
+        return request.post(self.endpoint(),
+                            request_body)["id"]
+
+    def delete(self, policy_id):
+        """Delete a policy by ID.  Success returns None"""
+        sanity.validate_object_id(policy_id)
+        request = HttpHelper(self.session)
+        delete_endpoint = "%s/%s" % (self.endpoint(), policy_id)
+        request.delete(delete_endpoint)
+        return None
+
+    def update(self, policy_body):
+        """Update a policy.  Success returns None"""
+
+        request = HttpHelper(self.session)
+        request_body = utility.policy_to_dict(policy_body)
+        policy_id = request_body[self.policy_key()]["id"]
+        sanity.validate_object_id(policy_id)
+        update_endpoint = "%s/%s" % (self.endpoint(), policy_id)
+        request.put(update_endpoint, request_body)
+        return None
