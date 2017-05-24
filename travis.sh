7c3e7606c4e4fc31b0d4d41398890694eabd2162
Hana Lee <hlee@cloudpassage.com>
2017-05-24 15:29:10 -0700

    test: added travis after_success

---
+++ travis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master"; then
+   docker run -it --rm -e HALO_API_KEY=$HALO_API_KEY -e HALO_API_SECRET_KEY=$HALO_API_SECRET_KEY -e HALO_API_HOSTNAME=$HALO_API_HOSTNAME -e HALO_API_PORT=$HALO_API_PORT -e CODECLIMATE_REPO_TOKEN=$CODECLIMATE_REPO_TOKEN cloudpassage_halo_python_sdk /bin/sh -c "cd cloudpassage/tests/; py.test ."
+else:
+   docker run -it --rm -e HALO_API_KEY=$HALO_API_KEY -e HALO_API_SECRET_KEY=$HALO_API_SECRET_KEY -e HALO_API_HOSTNAME=$HALO_API_HOSTNAME -e HALO_API_PORT=$HALO_API_PORT cloudpassage_halo_python_sdk /bin/sh -c "cd cloudpassage/tests/; py.test ."
+
+fi
